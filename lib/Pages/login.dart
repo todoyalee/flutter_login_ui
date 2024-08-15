7c30155319172920a4ee21b300c53c6e633c2d87
Mohamed Ali Belkouri <medali.b@devnet.me>
2024-08-15 14:53:54 +0100

    feat;chaing the design

--- lib/Pages/login.dart
+++ lib/Pages/login.dart
@@ -14,118 +14,133 @@ class _LoginState extends State<Login> {
         debugShowMaterialGrid: false,
         home: Scaffold(
           body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                TweenAnimationBuilder(
-                  tween: Tween<double>(begin: 0, end: 1),
-                  duration: Duration(microseconds: 100),
-                  builder: (BuildContext context, double value, Widget? child) {
-                    return Opacity(
-                      opacity: value,
-                      child: Padding(
-                        padding: EdgeInsets.all(20),
-                        child: child,
-                      ),
-                    );
-                  },
-                  child: Container(
-                    height: 180,
-                    width: 800,
-                    decoration: BoxDecoration(color: Colors.purple),
-                    child: Center(
-                      child: Text(
-                        " Welcome back",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 45),
+            child: Container(
+              color: Colors.purple,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  TweenAnimationBuilder(
+                    tween: Tween<double>(begin: 0, end: 1),
+                    duration: Duration(milliseconds: 5000),
+                    builder:
+                        (BuildContext context, double value, Widget? child) {
+                      return Opacity(
+                        opacity: value,
+                        child: Padding(
+                          padding: EdgeInsets.all(20),
+                          child: child,
+                        ),
+                      );
+                    },
+                    child: Container(
+                      height: 180,
+                      width: 800,
+                      decoration: BoxDecoration(color: Colors.purple),
+                      child: Center(
+                        child: Text(
+                          " Welcome back",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 45),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.lightBlue,
+                  SizedBox(
+                    height: 15,
                   ),
-                  child: Column(
-                    children: [
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Login",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                  fontSize: 20),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Login",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                    fontSize: 20),
+                              ),
+                            ]),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              prefix: Icon(Icons.person),
+                              label: Text("Email"),
+                              hintText: "Email",
                             ),
-                          ]),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(color: Colors.white),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            prefix: Icon(Icons.person),
-                            label: Text("Email"),
-                            hintText: "Email",
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(color: Colors.white),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              prefix: Icon(Icons.key),
-                              label: Text("Password"),
-                              hintText: "Password"),
+                        SizedBox(
+                          height: 10,
                         ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Checkbox(
-                              value: false,
-                              onChanged: (value) {
-                                //value= !value;
-                              }),
-                          Text(
-                            "Forgot Password ?",
-                            style: TextStyle(
-                                color: Colors.blue,
-                                fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      ),
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.purple),
-                          onPressed: () {},
-                          child: Text(
-                            "Login ",
-                            style: TextStyle(color: Colors.white),
-                          )),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        "Create account",
-                        style: TextStyle(color: Colors.purple),
-                      )
-                    ],
-                  ),
-                )
-              ],
+                        Container(
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20)),
+                                prefix: Icon(Icons.key),
+                                label: Text("Password"),
+                                hintText: "Password"),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Checkbox(
+                                value: false,
+                                onChanged: (value) {
+                                  //value = ! value;
+                                }),
+                            Text(
+                              "Forgot Password ?",
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                        Container(
+                          width: 300,
+                          height: 50,
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.purple),
+                              onPressed: () {},
+                              child: Text(
+                                "Login ",
+                                style: TextStyle(color: Colors.white),
+                              )),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Create account",
+                          style: TextStyle(
+                              color: Colors.purple,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ));
